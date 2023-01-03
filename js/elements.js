0e6b1486a92be6c27b54888758de159d339f86a4
Giselle Brasil Macedo <55713609+Gisellebm@users.noreply.github.com>
2023-01-03 19:09:26 -0300

    :lipstick: feat: Estilizaçao CSS do projeto

---
+++ js/elements.js
@@ -0,0 +1,23 @@
+const buttonPlay = document.getElementById(".play");
+const buttonStop = document.getElementById(".stop");
+const buttonPlus = document.getElementById(".plus");
+const buttonMinus = document.getElementById(".minus");
+const minutesDisplay = document.getElementById(".minutes");
+const secondsDisplay = document.getElementById(".seconds");
+const buttonForest = document.getElementById(".forest");
+const buttonRain = document.getElementById(".rain");
+const buttonCoffeeShop = document.getElementById(".coffeeShop");
+const buttonFireplace = document.getElementById(".fireplace");
+
+export {
+  buttonPlay,
+  buttonStop,
+  buttonPlus,
+  buttonMinus,
+  minutesDisplay,
+  secondsDisplay,
+  buttonForest,
+  buttonRain,
+  buttonCoffeeShop,
+  buttonFireplace
+};
